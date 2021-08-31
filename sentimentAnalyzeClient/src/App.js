e86f19b80d46dd0189ddfe49ed8df9c46d1e2ca8
capixm <74554154+capixm@users.noreply.github.com>
2021-08-31 13:26:55 +0200

    Update App.js
    Make changes to change the color of the sentiment response to green if the sentiment is positive, yellow if the sentiment is neutral and red if the sentiment is negative.

--- sentimentAnalyzeClient/src/App.js
+++ sentimentAnalyzeClient/src/App.js
@@ -49,9 +49,9 @@ class App extends React.Component {
         let output = data.label;
         let color = "white"
         switch(output) {
-          case "positive": color = "black";break;
-          case "negative": color = "black";break;
-          default: color = "black";
+          case "positive": color = "green";break;
+          case "neutral" : color = "yellow";break;
+          case "negative": color = "red";break;
         }
         output = <div style={{color:color,fontSize:20}}>{output}</div>
         this.setState({sentimentOutput:output});
